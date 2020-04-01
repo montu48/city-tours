f60c84fabf5ec8f09eadb73b6ee8648d07a75540
Meet Shah <montu48@gmail.com>
2020-04-01 08:23:58 +0530

    Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
 import Navbar from './components/Navbar/Navbar'
+import TourList from './components/TourList'
 
 function App() {
   return <React.Fragment>
     <Navbar />
+    <TourList />
   </React.Fragment>
 }
 
---
+++ src/components/TourList/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Tour from '../Tour'
+
+export default class TourList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello From TourList</h1>
+               <Tour />
+            </div>
+        )
+    }
+}
